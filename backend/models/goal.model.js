202d3bd03ce0562e6b1f4692551836ad7936912b
Pranay-Builds <contactpranay10@gmail.com>
2025-02-12 22:46:25 +0530

    Added database connection and the goal model

---
+++ backend/models/goal.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const goalSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    deadline: {
+        type: Date,
+        required: true
+    }
+}, { timestamps: true });
+
+export const goal = new mongoose.model("Goal", goalSchema)
+
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import { connectDB } from "./config/db.js";
 dotenv.config()
+
+connectDB();
+
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server started on http://localhost:"+process.env.PORT)
