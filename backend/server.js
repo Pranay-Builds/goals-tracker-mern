082c43571fa5a2d5ed96e9f864b4452f56d85fc3
Pranay-Builds <contactpranay10@gmail.com>
2025-02-17 14:48:56 +0530

    Added dist folder from npm run build and is ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import router from "./routes/goals.routes.js";
+import path from "path";
 dotenv.config()
 
 connectDB();
@@ -12,6 +13,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+    })
+}
 
 app.use('/api/goals', router);
 
